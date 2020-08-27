813ce601c91a8c65fab2fa22f4039ea6d510a138
Fabio-Antonio <16106054@ittlahuac.edu.mx>
2020-08-26 20:55:30 -0500

    contratoconsultas10

--- numero_de_contrato.php
+++ numero_de_contrato.php
@@ -23,7 +23,7 @@ $conn = null;
 $ch = curl_init();
 
 // definimos la URL a la que hacemos la petición
-curl_setopt($ch, CURLOPT_URL,"http://192.168.0.38:8888/besa/consulta_numero_contrato.php");
+curl_setopt($ch, CURLOPT_URL,"http://192.168.1.70:8888/besa/consulta_numero_contrato.php");
 // indicamos el tipo de petición: POST
 curl_setopt($ch, CURLOPT_POST, TRUE);
 // definimos cada uno de los parámetros

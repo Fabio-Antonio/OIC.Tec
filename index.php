17bdecba6c959eaf8542247b150bd99fade6d1f0
“jahir-creator <jahirfloresreyes20@gmail.com>
2020-09-27 11:32:48 -0500

    responsive

--- index.php
+++ index.php
@@ -1,221 +1,46 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-<link href="https://cdn.datos.gob.mx/assets/css/main.css" rel="stylesheet">
-<link href="https://cdn.datos.gob.mx/bower_components/polymer/polymer.html" rel="import">
-<link href="css/estilos.css" rel="stylesheet">
+<html lang="es-MX">
 
 <head>
-
-<link href="https://cdn.datos.gob.mx/assets/img/favicon.ico" rel="shortcut icon">
-<link href="https://cdn.datos.gob.mx/bower_components/dgm-footer/dgm-footer.html" rel="import">
-  <meta charset="UTF-8">
-
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-<meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Login Sistema BESA </title>
-
-    <style>
-@import url(https://fonts.googleapis.com/css?family=Exo:100,200,400);
-@import url(https://fonts.googleapis.com/css?family=Source+Sans+Pro:700,400,300);
-
-body{
-	margin: 0;
-	padding: 0;
-	background: #fff;
-
-	color: #fff;
-	font-family: Arial;
-	font-size: 12px;
-}
-
-.body{
-	position: absolute;
-	top: -20px;
-	left: -20px;
-	right: -40px;
-	bottom: -40px;
-	width: auto;
-	height: auto;
-	background-image: url(banner.jpg);
-	background-size: cover;
-	-webkit-filter: blur(5px);
-	z-index: 0;
-}
-.eye{
-  position:absolute;
-  height:200px;
-  width:600px;
-  top: 5px;
-  left : 400px;
-  z-index: 1;
-}
-
-.grad{
-	position: absolute;
-	top: -20px;
-	left: -20px;
-	right: -40px;
-	bottom: -40px;
-	width: auto;
-	height: auto;
-	background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(0,0,0,0)), color-stop(100%,rgba(0,0,0,0.65))); /* Chrome,Safari4+ */
-	z-index: 1;
-	opacity: -0.9;
-}
-
-.header{
-	position: absolute;
-	top: calc(50% - 35px);
-	left: calc(50% - 255px);
-	z-index: 2;
-}
-
-.header div{
-	float: left;
-	color: #fff;
-	font-family: 'Exo', sans-serif;
-	font-size: 35px;
-	font-weight: 200;
-}
-
-.header div span{
-	color: #FBFBEF !important;
-}
-
-.login{
-	position: absolute;
-	top: calc(50% - 75px);
-	left: calc(50% - 50px);
-	height: 150px;
-	width: 350px;
-	padding: 10px;
-	z-index: 2;
-}
-
-.login input[type=text]{
-	width: 250px;
-	height: 30px;
-	background: transparent;
-	border: 1px solid rgba(255,255,255,0.6);
-	border-radius: 2px;
-	color: #fff;
-	font-family: 'Exo', sans-serif;
-	font-size: 16px;
-	font-weight: 400;
-	padding: 4px;
-}
-
-.login input[type=password]{
-	width: 250px;
-	height: 30px;
-	background: transparent;
-	border: 1px solid rgba(255,255,255,0.6);
-	border-radius: 2px;
-	color: #fff;
-	font-family: 'Exo', sans-serif;
-	font-size: 16px;
-	font-weight: 400;
-	padding: 4px;
-	margin-top: 10px;
-}
-
-.login input[type=button]{
-	width: 260px;
-	height: 35px;
-	background: #fff;
-	border: 1px solid #fff;
-	cursor: pointer;
-	border-radius: 2px;
-	color: #a18d6c;
-	font-family: 'Exo', sans-serif;
-	font-size: 16px;
-	font-weight: 400;
-	padding: 6px;
-	margin-top: 10px;
-}
-
-.login input[type=button]:hover{
-	opacity: 0.8;
-}
-
-.login button[type=button]:active{
-	opacity: 0.6;
-}
-
-.login input[type=text]:focus{
-	outline: none;
-	border: 1px solid rgba(255,255,255,0.9);
-}
-
-.login input[type=password]:focus{
-	outline: none;
-	border: 1px solid rgba(255,255,255,0.9);
-}
-
-.login input[type=button]:focus{
-	outline: none;
-}
-
-::-webkit-input-placeholder{
-   color: rgba(255,255,255,0.6);
-}
-
-::-moz-input-placeholder{
-   color: rgba(255,255,255,0.6);
-}
-</style>
-
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Sistema B.E.S.A</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/estile.css">
 </head>
 
-<nav class="navbar navbar-inverse sub-navbar navbar-fixed-top" style="height: 55px; ">
-  <div class="container">
-    <div class="navbar-header" style="margin-top: 10px;">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#subenlaces">
-      <span class="sr-only">Interruptor de Navegación</span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="principal.php" id="besa2" >B.E.S.A</a>
-      <a> <img data-v-4a3754a3="" src="icons/lf.png" alt="logo gobierno de méxico" class="logos" style="width: 80%;height: 50%; margin-top: -100px; margin-bottom: -25px; margin-left: 420px "></a>
-    </div>
-
-    
-  </div>
-</nav>
-
-
-<body class="front">
-<main>
-
-  <div class="body"></div>
-		<div class="grad"></div>
-    <div style="position: relative; left: 80px; top: 0;">
-		<img src="icons/lf.png" class='eye' style="width: 500px; height: 210px; margin-top: 50px;  "/></div>
-		<div class="header">
-			<div><span> B.E.S.A </span></div>
-		</div>
-		<br>
-		<div class="login">
-
-        <form  role="form" id="formlg" action="login.php" method="POST">
-<div class="form-group">
-  <label class="control-label" for="email-01">Usuario:</label>
-<input class="form-control" id="email-01" type="text" pattern="[A-Za-z0-9_-]{1,8}" required placeholder="username" name="nombre">
-</div>
-	<div class="form-group">
-       <label class="control-label" for="password-01">Password:</label>
-
- <input class="form-control" id="password-01" type="password" placeholder="password" pattern="[A-Za-z0-9_-]{1,8}" required name="password">
-</div>
-				<button class="btn btn-primary btn-lg" type="submit">Entrar</button>
-        </form>
-		</div>
-
-</main>
+<body>
+    <header class="site-header">
+        <div class="contenedor encabezado">
+            <p>
+                B.E.S.A
+            </p>
+            <div class="imagen-header">
+                <img src="img/lf.png" alt="FuncionPublica">
+            </div>
+            <span></span>
+        </div>
+    </header>
+    <main class="login">
+        <div class="contenedor formularios-logo">
+            <h1>B.E.S.A</h1>
+            <form action="" class="formulario-logo">
+                <div class="campo">
+                    <label for="email-01">USUARIO</label>
+                    <input type="text" name="nombre" id="email-01" placeholder="Username">
+                </div>
+                <div class="campo">
+                    <label for="password-01">PASSWORD</label>
+                    <input type="password" name="password" id="password-01" placeholder="Password">
+                </div>
+                <div>
+                    <input type="submit" value="Entrar" class="btn btn-verde">
+                </div>
+            </form>
+        </div>
+    </main>
 </body>
-
-
 
 </html>

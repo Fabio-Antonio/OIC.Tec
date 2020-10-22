3b3fda991344286afd898432bfc0b46f7e1033c3
Fabio-Antonio <ing.fabio.a@gmail.com>
2020-10-22 15:01:01 -0500

    consolidado8

--- url.php
+++ url.php
@@ -1,4 +1,4 @@
 <?php
 //$path="https://".$_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT'];
-$path=$_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT'];
+ $path=$_SERVER['SERVER_NAME'].":".$_SERVER['SERVER_PORT'];
 ?>
